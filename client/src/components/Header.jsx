47265c8ea34ad1e8772bc2178825cb937a3d922c
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2025-01-05 12:30:14 +0530

    Header animations done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,68 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { motion } from "motion/react"
 
 const Header = () => {
   return (
-    <div className='flex flex-col justify-center items-center text-center my-20'>
-        <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
+    <motion.div
+    initial={{ opacity: 0,y:100 }}
+    transition={{ duration: 1 }}
+    whileInView={{ opacity: 1,y:0 }}    
+    viewport={{ once: true }}
+    className='flex flex-col justify-center items-center text-center my-20'>
+        <motion.div
+        initial={{ opacity: 0,y:-20 }}
+        animate={{ opacity: 1,y:0 }}    
+        transition={{ duration: 0.8, delay:0.2 }}
+        viewport={{ once: true }}
+         className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
             <p>Text to image Generator</p>
             <img src={assets.star_icon} alt="" />
-        </div>
+        </motion.div>
 
-        <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn any text to <span className='text-blue-600'>image</span> in a matter of seconds</h1>
-        <p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with AI. Take your Imagination to a next level by generating images using imagify. </p>
-        <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex  items-center gap-2 rounded-full '>Get Started <img className='h-6' src={assets.star_group}  /></button>
+        <motion.h1 
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1 }}
+        transition={{ duration:2, delay:0.4 }}
+        className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn any text to <span className='text-blue-600'>image</span> in a matter of seconds</motion.h1>
+        <motion.p 
+         initial={{ opacity: 0,y:20 }}
+        animate={{ opacity: 1, y:0 }}
+        transition={{ duration:0.8, delay:0.6 }}
+        className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with AI. Take your Imagination to a next level by generating images using imagify. </motion.p>
+
+        <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        
+         initial={{ opacity: 0}}
+        animate={{ opacity: 1 }}
+        transition={{default:{ duration:0.5},opacity:{ delay:0.8, duration:1} }}
+         className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex  items-center gap-2 rounded-full '>Get Started <img className='h-6' src={assets.star_group}  /></motion.button>
 
 
-        <div className='flex gap-2 mt-10'>
+        <motion.div className='flex gap-2 mt-10'
+          initial={{ opacity: 0}}
+        animate={{ opacity: 1 }}
+        transition={{ duration:1, delay:1 }}
+        >
+      
             {
                 Array(6).fill('').map((item,index)=>(
-                    <img key={index} width={70} className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' src={index % 2 ===0 ? assets.sample_img_2 : assets.sample_img_1 } alt="" />
+                    <motion.img
+                    whileHover={{ scale: 1.05, duration:0.1 }}
+                     key={index} width={70} className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' src={index % 2 ===0 ? assets.sample_img_2 : assets.sample_img_1 } alt="" />
 
                 ))
             }
-        </div>
+        </motion.div>
 
-        <p className='mt-2 text-neutral-500'>Generated images from Imagify</p>
-    </div>
+        <motion.p
+          initial={{ opacity: 0}}
+        animate={{ opacity: 1 }}
+        transition={{ duration:0.8, delay:1.2 }}
+         className='mt-2 text-neutral-500'>Generated images from Imagify</motion.p>
+    </motion.div>
   )
 }
 
