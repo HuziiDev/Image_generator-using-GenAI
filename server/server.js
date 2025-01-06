44e0147b1a22f087a982da49e761c8595dfcfc05
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2025-01-06 12:24:25 +0530

    Database connection done

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongodb.js'
 
 const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+ connectDb()
 app.get('/', (req,res)=>{
     res.send("Hello World")
 })
