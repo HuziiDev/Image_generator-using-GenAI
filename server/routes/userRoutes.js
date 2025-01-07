b001e1c45da3a6944ab84514a933e7836e85bd6b
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2025-01-07 12:27:38 +0530

    Backend complete

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import {registerUser, loginUser} from '../controllers/userController.js'
+import {registerUser, loginUser, userCredits} from '../controllers/userController.js'
+import userAuth from '../middlewares/auth.js';
 const userRouter = express.Router()
 // Route for user registration
 userRouter.post('/register', registerUser);
 
 // Route for user login
 userRouter.post('/login', loginUser);
+userRouter.post('/credits', userAuth, userCredits)
 
 
 export default userRouter
--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDb from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/api/user', userRouter)
+app.use('/api/image', imageRouter)
 
 await connectDb()
 
